40393ccb4944adf0b8c4b15b6379ceb2581649df
Emeka Ugwuanyi <emylincon@gmail.com>
2019-04-30 20:13:03 +0100

    finished the barchart

--- ploting.py
+++ ploting.py
@@ -9,6 +9,7 @@ ax2 = fig.add_subplot(233)
 ax3 = fig.add_subplot(234)
 ax4 = fig.add_subplot(236)
 ax5 = fig.add_subplot(132)
+#ax6 = fig.add_subplot(313)
 
 
 def plt1():
@@ -63,14 +64,31 @@ def plt5():
     plt.subplot(ax5)
 
 
+def plot_performance():
+    H = r.randrange(1, 100)
+    M = r.randrange(1, 100)
+    MH = r.randrange(1, 100)
+    re_use = r.randrange(1, 100)
+
+    name = ['Hits', 'Misses', 'Co-operative-Hits', 'Algo use']
+    ypos = ([0, 1, 2, 3])
+    values = [H, M, (H + MH), re_use]
+    ax1.set_xticks(ypos)
+    ax1.set_xticklabels(name)
+    ax1.bar(ypos, values, align='center', color='m')
+    ax1.set_title('Cache Performance')
+    plt.subplot(ax1)
+
+
 def plot_test():
 
     try:
-        plt1()
+        #plt1()
         plt2()
         plt3()
         plt4()
         plt5()
+        plot_performance()
         fig.suptitle('Testing subplot')
     except Exception as e:
         print(e)
