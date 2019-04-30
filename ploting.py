60a7936ae3ec04a15fb19cfca924bb4c2c69e22b
Emeka Ugwuanyi <emylincon@gmail.com>
2019-04-30 16:19:57 +0100

    fixed bug with randrange

--- ploting.py
+++ ploting.py
@@ -13,7 +13,7 @@ ax5 = fig.add_subplot(313)
 
 def plt1():
     x = list(range(10, 30))
-    y = [r.randint(i) for i in x]
+    y = [r.randrange(i) for i in x]
     ax1.grid(True, color='k')
     ax1.plot(x, y, linewidth=5, color='g')
     ax1.set_title('plt1')
@@ -23,7 +23,7 @@ def plt1():
 
 def plt2():
     x = list(range(10, 30))
-    y = [r.randint(i) for i in x]
+    y = [r.randrange(i) for i in x]
     ax2.grid(True, color='k')
     ax2.plot(x, y, linewidth=5, color='c')
     ax2.set_title('plt1')
@@ -33,7 +33,7 @@ def plt2():
 
 def plt3():
     x = list(range(10, 30))
-    y = [r.randint(i) for i in x]
+    y = [r.randrange(i) for i in x]
     ax3.grid(True, color='k')
     ax3.plot(x, y, linewidth=5, color='m')
     ax3.set_title('plt1')
@@ -43,7 +43,7 @@ def plt3():
 
 def plt4():
     x = list(range(10, 30))
-    y = [r.randint(i) for i in x]
+    y = [r.randrange(i) for i in x]
     ax4.grid(True, color='k')
     ax4.plot(x, y, linewidth=5, color='k')
     ax4.set_title('plt1')
@@ -53,7 +53,7 @@ def plt4():
 
 def plt5():
     x = list(range(51, 58))
-    val = [r.randint(i) for i in x]
+    val = [r.randrange(i) for i in x]
     explode = []
     for v in val:
         if v == max(val):
@@ -84,5 +84,4 @@ def draw_loop():
         drawnow(plot_test)
 
 
-if __name__ == "__main__":
-    draw_loop()
+draw_loop()
