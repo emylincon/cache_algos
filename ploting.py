af42c3aea191f127bcf382d88726f85024558a1c
Emeka Ugwuanyi <emylincon@gmail.com>
2019-04-30 18:43:21 +0100

    plotting module done

--- ploting.py
+++ ploting.py
@@ -4,11 +4,11 @@ import random as r
 import time
 
 fig = plt.figure()
-ax1 = fig.add_subplot(331)
-ax2 = fig.add_subplot(332)
-ax3 = fig.add_subplot(333)
-ax4 = fig.add_subplot(334)
-ax5 = fig.add_subplot(313)
+ax1 = fig.add_subplot(231)
+ax2 = fig.add_subplot(233)
+ax3 = fig.add_subplot(234)
+ax4 = fig.add_subplot(236)
+ax5 = fig.add_subplot(132)
 
 
 def plt1():
@@ -17,7 +17,6 @@ def plt1():
     ax1.grid(True, color='k')
     ax1.plot(x, y, linewidth=5, color='g')
     ax1.set_title('plt1')
-    ax1.legend()
     plt.subplot(ax1)
 
 
@@ -27,7 +26,6 @@ def plt2():
     ax2.grid(True, color='k')
     ax2.plot(x, y, linewidth=5, color='c')
     ax2.set_title('plt1')
-    ax2.legend()
     plt.subplot(ax2)
 
 
@@ -37,7 +35,6 @@ def plt3():
     ax3.grid(True, color='k')
     ax3.plot(x, y, linewidth=5, color='m')
     ax3.set_title('plt1')
-    ax3.legend()
     plt.subplot(ax3)
 
 
@@ -47,7 +44,6 @@ def plt4():
     ax4.grid(True, color='k')
     ax4.plot(x, y, linewidth=5, color='k')
     ax4.set_title('plt1')
-    ax4.legend()
     plt.subplot(ax4)
 
 
@@ -64,6 +60,7 @@ def plt5():
     cols = ['r', 'g', 'c', 'k', 'b', 'm', 'y']
     ax5.pie(val, labels=keys, autopct='%.3f%%', shadow=True, explode=explode, colors=cols)
     ax5.set_title('Relative Frequency')
+    plt.subplot(ax5)
 
 
 def plot_test():
